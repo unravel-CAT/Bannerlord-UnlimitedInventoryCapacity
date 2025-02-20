8bb765387e93ada9ffa411318ceaa53bd23d16af
unravel-CAT <1744475144@qq.com>
2025-02-20 14:29:55 +0800

    2025-01-20

---
+++ UnlimitedInventoryCapacity/InventoryCapacityPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.Library;
+
+namespace UnlimitedInventoryCapacity
+{
+    [HarmonyPatch(typeof(DefaultInventoryCapacityModel), "CalculateInventoryCapacity")]
+    public class InventoryCapacityPatch
+    {
+        private static bool Prefix(ref ExplainedNumber __result)
+        {
+            ExplainedNumber explainedNumber = new ExplainedNumber(Main.InventoryCapacity);
+            __result = explainedNumber;
+            return false;
+        }
+    }
+}
---
+++ UnlimitedInventoryCapacity/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using Newtonsoft;
+using TaleWorlds.MountAndBlade;
+using Newtonsoft.Json;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using Module = TaleWorlds.MountAndBlade.Module;
+
+namespace UnlimitedInventoryCapacity
+{
+    public class Main : MBSubModuleBase
+    {
+        public static int InventoryCapacity = 999999999;
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            try
+            {
+                new Harmony("UnlimitedInventoryCapacity").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                InformationManager.DisplayMessage(new InformationMessage("UnlimitedInventoryCapacity error"));
+            }
+        }
+    }
+}
